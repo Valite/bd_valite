7e99ac39953b607ed7b4e5bad11caa9432e7f18b
Valite <44102447+Valite@users.noreply.github.com>
2021-03-28 15:06:05 +0200

    1.3.5

--- login/config.php
+++ login/config.php
@@ -1,8 +1,12 @@
 <?php
-	$servidor="localhost";
-	$usuario="root";
-	$clave="";
-	$baseDeDatos="BD_PELICULAS";
+	/*$servidor="localhost";*/
+	$servidor="eu-cdbr-west-01.cleardb.com";
+	/*$usuario="root";*/
+	$usuario="b2e9d5b41af2da";
+	/*$clave="";*/
+	$clave="9f03ac44";
+	/*$baseDeDatos="BD_PELICULAS";*/
+	$baseDeDatos="heroku_1a05ed0c5d22f8f";
 
 	$enlace=mysqli_connect($servidor,$usuario,$clave,$baseDeDatos);
 
