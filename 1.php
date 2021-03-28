7be7e4940d757fa64d4e3b99d97bd9fa02d5230a
Valite <44102447+Valite@users.noreply.github.com>
2021-03-28 15:55:52 +0200

    1.3

--- 1.php
+++ 1.php
@@ -64,45 +64,45 @@ require_once "login/config.php";
 						<div class="inner">
 						<section>
 							<table>
-				<tr>
-					<th>ID</th>
-					<th>Nombre</th>
-					<th>Duracion</th>
-					<th>Año</th>
-					<th>Genero</th>
-					<th>Visto</th>
-				</tr>
-				<?php
-				  $consulta = "SELECT PELICULAS.id,PELICULAS.nombre,PELICULAS.duracion,PELICULAS.ano,GENEROS.nombre,VISTO.nombre
-											FROM PELICULAS,VISTO,GENEROS
-											WHERE GENEROS.id = PELICULAS.genero AND VISTO.id = PELICULAS.visto";
-					$ejecutarConsulta = mysqli_query($enlace, $consulta);
-					$verFilas = mysqli_num_rows($ejecutarConsulta);
-					$fila = mysqli_fetch_array($ejecutarConsulta);
+								<tr>
+									<th>ID</th>
+									<th>Nombre</th>
+									<th>Duracion</th>
+									<th>Año</th>
+									<th>Genero</th>
+									<th>Visto</th>
+								</tr>
+								<?php
+								$consulta = "SELECT PELICULAS.id,PELICULAS.nombre,PELICULAS.duracion,PELICULAS.ano,GENEROS.nombre,VISTO.nombre
+															FROM PELICULAS,VISTO,GENEROS
+															WHERE GENEROS.id = PELICULAS.genero AND VISTO.id = PELICULAS.visto";
+									$ejecutarConsulta = mysqli_query($enlace, $consulta);
+									$verFilas = mysqli_num_rows($ejecutarConsulta);
+									$fila = mysqli_fetch_array($ejecutarConsulta);
 
-					if(!$ejecutarConsulta){
-						echo"Error en la consulta";
-					}else{
-						if($verFilas<1){
-							echo"<tr><td>Sin registros</td></tr>";
-						}else{
-							for($i=0; $i<=$fila; $i++){
-								echo'
-									<tr>
-										<td>'.$fila[0].'</td>
-										<td>'.$fila[1].'</td>
-										<td>'.$fila[2].'</td>
-										<td>'.$fila[3].'</td>
-										<td>'.$fila[4].'</td>
-										<td>'.$fila[5].'</td>
-									</tr>
-								';
-								$fila = mysqli_fetch_array($ejecutarConsulta);
-							}
-						}
-					}
-				?>
-			</table>
+									if(!$ejecutarConsulta){
+										echo"Error en la consulta";
+									}else{
+										if($verFilas<1){
+											echo"<tr><td>Sin registros</td></tr>";
+										}else{
+											for($i=0; $i<=$fila; $i++){
+												echo'
+													<tr>
+														<td>'.$fila[0].'</td>
+														<td>'.$fila[1].'</td>
+														<td>'.$fila[2].'</td>
+														<td>'.$fila[3].'</td>
+														<td>'.$fila[4].'</td>
+														<td>'.$fila[5].'</td>
+													</tr>
+												';
+												$fila = mysqli_fetch_array($ejecutarConsulta);
+											}
+										}
+									}
+								?>
+							</table>
 						</section>
 					</div>
 				</div>
